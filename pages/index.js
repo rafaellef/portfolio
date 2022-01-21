35f1b482aa0fad54f22163c7bead9f60cfb4c3ff
rafaellef <rafaellemosfernandes@gmail.com>
2022-01-21 12:33:13 -0300

    Foto do perfil adicionada

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="blue" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
                 Bem vindo!
             </Box>
 
-            <Box display={{md:'flex'}}>
+            <Box display={{ md: 'flex' }}>
                 <Box flexGrow={1}>
                     <Heading as="h2" variant="page-title">
                         Rafael Fernandes
                     </Heading>
                     <p>Desenvolvedor Front-End</p>
+                </Box>
+                <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/rafael.png" alt="foto do perfil" />
                 </Box>
             </Box>
         </Container>
